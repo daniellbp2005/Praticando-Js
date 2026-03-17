1b2a363fb56d4769d2f370f4f63ca84c1aff56b1
daniellbp2005 <daniellbp2005@gmail.com>
2026-03-17 15:17:48 -0300

    Primero Commit

---
+++ script.js
@@ -0,0 +1,73 @@
+function Textoo(){
+    const nome = "Daniel Lozano";
+    var idade = 20;
+    let profissao = "Estudando"
+    cidade = "Scs"
+
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos, trabalho como ${profissao} e moro em ${cidade}.`)
+}Textoo();
+
+function produto(){
+    let produto = "fone de ouvido"
+    let preco = 20.00
+    let qtd = 1
+
+    console.log(`Comprei ${produto} unidades de ${preco} por R$${qtd} cada.`)
+}produto()
+    
+function soma(){
+    let1 = 10
+    let2 = 20
+    console.log("10 + 20 =",(let1 + let2))
+    console.log("10 - 20 =",(let1 - let2))
+    console.log("10 * 20 =",(let1 * let2))
+    console.log("10 / 20 =",(let1 / let2))
+}soma()
+
+
+function tipos(){
+    const nome = "Daniel Lozano";
+    var idade = 20;
+    let profissao = "Estudando"
+    cidade = "Scs"
+    escola = "Senai"
+
+    
+    console.log(nome, "-", typeof nome)
+    console.log(idade, "-", typeof idade)
+    console.log(profissao, "-", typeof profissao)
+    console.log(cidade, "-", typeof cidade)
+    console.log(escola, "-", typeof escola)
+    
+}tipos()
+
+function coverter(){
+    let num = "100"
+    let numm = 100
+    console.log(typeof num)
+
+    
+    let numconvertido = Number(num)
+    console.log(typeof numconvertido)
+
+    let nummconv = String(numm)
+    console.log(typeof nummconv)
+    
+}coverter()
+
+function desafio(){
+    nome = "Daniel"
+    idade = 20
+    altura = "Sei não"
+    peso = "Sei não"
+    cidade = "Scs"
+    profissao ="Estudante"
+
+
+    console.log("Nome: ",nome," \n")
+    console.log("Idade: ",idade," \n")
+    console.log("Altura: ",altura," \n")
+    console.log("peso: ",peso," \n")
+    console.log("cidade: ",cidade," \n")
+    console.log("profissao: ",profissao," \n")
+}desafio();
