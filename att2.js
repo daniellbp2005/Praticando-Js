946b35423a0a37995b7d638c4742b104d531b13a
daniellbp2005 <daniellbp2005@gmail.com>
2026-03-18 17:21:26 -0300

    :feat: js: att da aula

---
+++ att2.js
@@ -0,0 +1,136 @@
+function valorPedido(){
+    console.log("=== Fretadora === \n\n");
+    let valorCompra = Number(prompt(`Digite o Valor do Frete`))
+
+    if (valorCompra === 100){
+        console.log(`O valor do Frete é grátis`);
+    } else if(valorCompra === 50){
+        console.log(`O valor do Frete é 10R$`);
+
+    }else {
+        console.log(`O valor do Frete é 20R$`);
+    }
+}
+
+function idadeConvite(){
+    let idade = Number(prompt(`Digite sua idade: `));
+    let convite = prompt(`Digite c vc tem Convite: `);
+
+    if (idade >= 18 && convite == "sim" || convite == "Sim"){
+        console.log("Pode entrar");
+    } else{
+        console.log("Não pode entrar");
+    }
+}
+function notas(){
+    let nota = prompt(`Digite sua Nota: `)
+    if(nota >= 90){
+        console.log("Conceito A");
+    }else if( nota < 90 && nota >= 80){
+        console.log("Conceito B");
+    }else if(nota < 80 && nota >= 70){
+        console.log("Conceito C");
+    }else if(nota < 70 && nota >= 60){
+        console.log("Conceito D");
+    }else{
+        console.log("Conceito F");
+    }
+}
+
+function IMC (){
+    let peso = Number(prompt(`Digite seu peso em kilos:`))
+    let altura = Number(prompt(`Digite sua Altura em metros (ex: 1.75)`))
+
+    let altQuadrado = altura * altura;
+    let conta =  peso/altQuadrado;
+
+    if(conta >= 25.00){
+        console.log("Vc está com Sobrepeso");
+    }else if(conta > 18.5 && conta <= 24.9){
+        console.log("Vc está com o peso Normal");
+    }else{
+        console.log("Vc está abaxo do peso");
+    }
+}
+
+function estoque(){
+    let estoque = Number(prompt(`Digite a quantidade de produtos disponiveis`))
+    if(estoque > 10){
+        console.log("Produto Disponovel");
+        
+    }else if(estoque >= 1 && estoque <= 10){
+        console.log("Últimas unidades");
+    }else{
+        console.log("Esgotado");
+    }
+}
+// let idade = prompt(`Digite uma idade`);
+// if(idade >= 18){
+//     console.log("maior d idade");
+// }else {
+//     console.log("menor d idade");
+// }
+
+
+// let numero = prompt(`Digite um NUmero`);
+// if ( numero <= 0){
+//     console.log("Negativo");
+// } else{
+//     console.log("Positivo");
+// }
+
+// let nota = prompt(`Digite uma nota`);
+// if( nota >= 60){
+//     console.log("Aprovadp");
+// }else{
+//     console.log("Reprovado");
+// }
+
+// let num = prompt(`Digite um numero`);
+// if(num == 0){
+//     console.log("Ele é zero");
+// }else if(num >=1){
+//     console.log("Ele é positivo");
+// }else{
+//     console.log("Ele é negativo");
+// }
+
+// let idade1 = prompt(`Digite sua idade`);
+// if(idade1 < 13){
+//     console.log("Crinça");
+// }else if( idade1 >= 13 && idade1 < 18){
+//     console.log("Adolescente");
+// }else{
+//     console.log("Adulto");
+// }
+
+
+// let nume = prompt(`Digite um numero`);
+// if(nume% 2 == 0){
+//     console.log("Par");
+// }else {
+//     console.log("Impar");
+// }
+
+// let ope = prompt(`Digite uma operação`);
+// let nume1 = Number(prompt(`Digite um numero`))
+// let nume2 = Number(prompt(`Digite outro numero`))
+
+// if(ope == "+"){
+//     console.log(nume1 + nume2);
+// }else if(ope =="-"){
+//     console.log(nume1 - nume2);
+// }else if(ope =="*"){
+//     console.log(nume1 * nume2);
+// }else{
+//     console.log(nume1 / nume2);
+// }
+
+let maior1 = Number(prompt(`Digite o valor da compra`));
+
+if(maior1 >= 100){
+    let calca = maior1/10;
+    console.log(`O valor do desconto foi ${calca}`);
+}else{
+    console.log("Valor p descoto n atingido");
+}
--- miniTerminal.js
+++ miniTerminal.js
@@ -14,7 +14,7 @@ function lanche(){
     console.log(`Total dacompra: ${unitario * qtd}\n`)
     console.log(`Pedido realizado com sucesso! \nObrigado pela preferência.\n \n`)
 
-}lanche();
+}
 
 function desafio(){
     let nome = "Livraria"
@@ -32,4 +32,90 @@ function desafio(){
     console.log(`Total dacompra: ${unitario * qtd}\n`)
     console.log(`Pedido realizado com sucesso! \nObrigado pela preferência.\nJajá chega em sua casa\n`)
 
-}desafio();
+}
+
+function iniciar (){
+    console.log("===== Bem Vindo a Escola ===\n\n");
+    let num1 = Number(prompt("Digite a sua Primeira nota\n"));
+    let num2 = Number(prompt("Digite a sua Segunda nota\n"));   
+    let num3 = Number(prompt("Digite a sua Terceira nota\n\n"));
+    let soma = (num1 + num2 + num3)/3
+    let media = Number(soma.toFixed(2))
+
+    console.log("=== Sua Media do Sementre foi ====\n");
+    console.log(`=== Nota 1: ${num1} `);
+    console.log(`=== Nota 2: ${num2} `); 
+    console.log(`=== Nota 3: ${num3} `);
+    if (soma >= 6){
+        console.log(`Vc passou de ano sua media foi ${media}\n`);
+        console.log(`Parabens !!!`);
+    } else{
+        console.log(`Vc não consegui, tente novamene ${media}`);
+    }
+}
+
+function tem(){
+    let temp = Number(prompt("===-- Digite a Tempeatura de Hoje --==="));
+
+    if(temp < 20){
+        alert(`Está Frio`);
+    } else{
+        alert(`Está Calor`);
+    }
+}
+
+// function lista(){ // percorrendo um array com for
+//     let lista = ["banana","maça","laranja","uva","pera"]
+
+//     console.log("===-- Lista de Frutas ===\n\n");
+//     for (let item of lista){
+
+//         console.log(`Fruta : ${item}`);
+//     }
+// }lista();
+
+// function acesso(){
+//     let tipo_user = prompt("Digite quem é vc: ")
+
+//     if (tipo_user == "admin"){
+//         console.log("\n\nliberado",tipo_user);
+//     } else if(tipo_user == "cliente"){
+//         console.log("\n\nLiberado",tipo_user);
+//     } else {
+//         console.log("\n\ninvalido");
+//     }
+// }acesso()
+
+
+// let diferente = (5 != 3); //true
+// tem como eu inverter tudo dentro, usand um exclamação antes do parenteses, ex:!(5 != 3), ele retoran false.
+
+
+    let tema = prompt("Digite o tema que vc quer: claro, escuro ou azul");
+
+    switch(tema) {
+        case "claro":
+            console.log("Tema att");
+            break;
+        case "escuro":
+            console.log("Tema att");
+            break;
+        default:
+            console.log("Tema invalido");
+    }
+
+
+function inpusts(){
+    let email = document.getElementById("email");
+    let input = document.getElementById("nome");
+    let senha = document.getElementById("senha");
+
+    if(input == ""){
+        console.log("Tente dnovo")
+    }
+
+
+    botao.addEventListener("enviar", () => {
+        console.log("Funcionou")
+})
+}
