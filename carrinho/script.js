554879a7f9b08e2291982403d17932434ba2d72f
daniellbp2005 <daniellbp2005@gmail.com>
2026-03-25 17:24:50 -0300

    terminando mais uma aula de JS

--- carrinho/script.js
+++ carrinho/script.js
@@ -1,4 +1,3 @@
-
 let caixa = document.getElementById("caixa");
 let titulo = document.getElementById("titulo");
 let btnAdd = document.getElementById("btn-add");
@@ -7,6 +6,14 @@ let lista = document.getElementById("lista");
 
 btnAdd.addEventListener("click", ()=>{
     const li = document.createElement("li");
-    li.innerText = "batata"
+    li.innerText = "batata";
     lista.appendChild(li);
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    li.appendChild(button);
+})
+
+const apagar = document.getElementById("excluir");
+    btnAdd.addEventListener("click", ()=>{
+    apagar.removeChild(apagar)
 })
