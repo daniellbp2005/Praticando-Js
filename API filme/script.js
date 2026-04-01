9d6145b64213c4dc8264614280f741b3e7f68eb3
daniellbp2005 <daniellbp2005@gmail.com>
2026-04-01 17:02:19 -0300

    Página de filmes com API

---
+++ API filme/script.js
@@ -0,0 +1,79 @@
+const campoBusca = document.getElementById("campo-busca");
+const listaFilmes = document.getElementById("lista-filmes");
+
+const API_KEY = "b605a85583ac4057349ab91c8f72c575";
+
+const URL_BASE = "https://api.themoviedb.org/3";
+const URL_IMAGEM = "https://image.tmdb.org/t/p/w500";
+
+//mostrar resultados
+
+function mostrarResultados(lista) {
+    listaFilmes.innerHTML = "";
+    lista.forEach(item => {
+        if (!item.poster_path) { // poster_path = caminho da img, é como vem da API
+            return;
+        }
+
+        const titulo = item.title || item.name;
+        const nota = item.vote_average || "N/A"; //media de notas
+
+        const div = document.createElement("div");
+        div.classList.add("filme")              //add um aclasse para a div criada
+        div.innerHTML =
+            `
+        <img src="${URL_IMAGEM + item.poster_path}" alt"${titulo}">
+        <div class="info">
+            <h3>${titulo}</h3>
+            <span>⭐ ${nota}</span>
+        </div>
+        `
+
+        listaFilmes.appendChild(div)
+    });
+}
+
+
+//buscar filmes e séries
+
+async function buscarTitulos(termoDigitado) {
+    try{//permite o sistema fazzer a busca, enq roda e n va travar
+        const resposta = await fetch(
+            `${URL_BASE}/search/multi?api_key=${API_KEY}&query=${termoDigitado}&language=pt-BR`
+        )
+
+        const dados = await resposta.json();
+        mostrarResultados(dados.results);
+    } catch (erro) {
+        console.error("Erro na busca: ",erro)
+    }
+}
+
+//buscar filmes populares
+
+async function buscarPopulares() {
+    try{
+        const resposta = await fetch(
+            `${URL_BASE}/movie/popular?api_key=${API_KEY}&language=pt-BR`
+        )
+        const dados = await resposta.json();
+        mostrarResultados(dados.results)
+    } catch (erro) {
+        console.error("Erro ao carregar populares: ",erro)
+    }
+}
+
+//add evento p carregar o conteudo popular assim q a página abrir
+window.addEventListener("load",() => {
+    buscarPopulares()
+})
+
+let timeout;
+campoBusca.addEventListener("input", ()=>{
+    clearTimeout(timeout);
+
+    timeout = setTimeout(()=>{
+        const termo =campoBusca.value;
+        buscarTitulos(termo);
+    }),500;
+})
--- buscaProduto/script.js
+++ buscaProduto/script.js
@@ -3,94 +3,121 @@ const campoProduto = document.getElementById("produto");
 const campoNome = document.getElementById("nome");
 const campoPreco = document.getElementById("preco");
 const campoCat = document.getElementById("categoria");
-
+const texto = document.getElementById("textoBaixo")
+const img = document.getElementById("imagemProd")
+const form = document.querySelector(".formulario")
+const novoTexto = document.querySelector(".novoTexto")
 
 const Mockados = {
     "teclado gamer": {
         id: 1,
         nome: "teclado gamer",
         preco: 120.90,
-        categoria: "Periféricos"
+        categoria: "Periféricos",
+        imagem: "https://picsum.photos/200/150?random=1"
     },
     "mouse sem fio": {
         id: 2,
         nome: "mouse sem fio",
         preco: 89.90,
-        categoria: "Periféricos"
+        categoria: "Periféricos",
+        imagem: "https://picsum.photos/200/150?random=2"
     },
     "monitor 24 polegadas": {
         id: 3,
         nome: "monitor 24 polegadas",
         preco: 799.90,
-        categoria: "Monitores"
+        categoria: "Monitores",
+        imagem: "https://picsum.photos/200/150?random=3"
     },
     "headset usb": {
         id: 4,
         nome: "headset usb",
         preco: 149.90,
-        categoria: "Audio"
+        categoria: "Audio",
+        imagem: "https://picsum.photos/200/150?random=4"
     },
     "notebook": {
         id: 5,
         nome: "notebook",
         preco: 3299.90,
-        categoria: "Computadores"
+        categoria: "Computadores",
+        imagem: "https://picsum.photos/200/150?random=5"
     },
     "webcam hd": {
         id: 6,
         nome: "webcam hd",
         preco: 199.90,
-        categoria: "Acessórios"
+        categoria: "Acessórios",
+        imagem: "https://picsum.photos/200/150?random=6"
     },
     "mousepad rgb": {
         id: 7,
         nome: "mousepad rgb",
         preco: 59.90,
-        categoria: "Periféricos"
+        categoria: "Periféricos",
+        imagem: "https://picsum.photos/200/150?random=7"
     },
     "teclado mecânico": {
         id: 8,
         nome: "teclado mecânico",
         preco: 299.90,
-        categoria: "Periféricos"
+        categoria: "Periféricos",
+        imagem: "https://picsum.photos/200/150?random=8"
     },
     "monitor curvo 27 polegadas": {
         id: 9,
         nome: "monitor curvo 27 polegadas",
         preco: 1299.90,
-        categoria: "Monitores"
+        categoria: "Monitores",
+        imagem: "https://picsum.photos/200/150?random=9"
     },
     "fone de ouvido bluetooth": {
         id: 10,
         nome: "fone de ouvido bluetooth",
         preco: 249.90,
-        categoria: "Audio"
+        categoria: "Audio",
+        imagem: "https://picsum.photos/200/150?random=10"
     }
 }
-
 
 function preencherCampos(dados) {
     campoNome.value = dados.nome;
     campoPreco.value = dados.preco;
     campoCat.value = dados.categoria;
+    img.src = dados.imagem;
+
+    img.onerror = () => {
+        let novoTexto = document.createElement("p")
+        form.appendChild(novoTexto);
+        novoTexto.innerText = "Imagem não encontrada";
+        novoTexto.style.color = "red";
+        novoTexto.style.fontWeight = 600;
+        img.remove()
+    }
 }
 
 function limparCampos() {
-    campoNome.value = "";
+    campoNome.value;
     campoPreco.value = "";
     campoCat.value = "";
+    img.src = "";
 }
 
 function buscarProduto() {
     const prod = campoProduto.value.trim().toLowerCase();
-
     const dadoProd = Mockados[prod]
 
     if (!dadoProd) {
-        alert("Produto não encontrado");
+        texto.innerText = "Produto não Encontrado"
+        texto.style.color = "red"
+        texto.style.fontWeight = 600
         limparCampos();
         return;
     }
+    texto.innerText = "Produto Encontrado"
+    texto.style.color = "green"
+    texto.style.fontWeight = 600
 
     preencherCampos(dadoProd);
 }
